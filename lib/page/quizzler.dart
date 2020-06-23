20c1950d8b655fe7bbd0aa5a8f2f0a498e15f483
Woogear Kwon <techkwon1717@hanmail.net>
2020-06-23 23:55:00 +0900

    Add a list of sample questions

--- lib/page/quizzler.dart
+++ lib/page/quizzler.dart
@@ -23,6 +23,16 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+
+  List<String> questionse = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
+  ];
+
+  int _questionNumb = 0;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -35,7 +45,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questionse[_questionNumb],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -59,7 +69,9 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                setState(() {
+                  if (_questionNumb < questionse.length - 1) _questionNumb++;
+                });
               },
             ),
           ),
@@ -82,7 +94,10 @@ class _QuizPageState extends State<QuizPage> {
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: <Widget>[
+          ],
+        )
       ],
     );
   }
