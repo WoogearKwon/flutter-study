a4c98e8a26b2500b44df573980f62eee5f1b00bb
Woogear Kwon <techkwon1717@hanmail.net>
2020-07-11 13:52:48 +0900

    add permissions and test geoLocator package

--- lib/page/clima/screens/loading_screen.dart
+++ lib/page/clima/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,13 +7,39 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+
+  void getLocation() async {
+    // LocationAccuracy: more accurate the location you're trying to get, the more battery intensive it will be
+    // async keyword gives us access to await keyword which awaits time-consuming work to be done before continuing next line of code
+    // make sure that your device's GPS is on
+    try {
+      Position position = await Geolocator()
+          .getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+      print(position);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  // initState() only gets called once at the state gets initialized gets created.
+  @override
+  void initState() {
+    super.initState();
+
+    getLocation();
+  }
+
+  // build() gets called when widgets gets build onto the screen,
+  // and gets called every single time that the widgets rebuild.
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFF0A0E21),
       body: Center(
         child: RaisedButton(
           onPressed: () {
             //Get the current location
+            getLocation();
           },
           child: Text('Get Location'),
         ),
