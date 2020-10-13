76321ae93f73a57d4e8d020cb8ac5243e51ca5fc
Woogear Kwon <techkwon1717@hanmail.net>
2020-10-13 22:52:33 +0900

    Todoey: Add Checkbox Listview and BottomSheet Widget

--- lib/page/todoey/screens/tasks_screen.dart
+++ lib/page/todoey/screens/tasks_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
+import '../widgets/tasks_list.dart';
+import 'add_task_screen.dart';
+
 class TasksScreen extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,6 +13,10 @@ class TasksScreen extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.lightBlueAccent,
         child: Icon(Icons.add),
+        onPressed: () {
+          // show bottom sheet
+          showModalBottomSheet(context: context, builder: (context) => AddTaskScreen());
+        },
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -50,12 +58,14 @@ class TasksScreen extends StatelessWidget {
           ),
           Expanded(
             child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
               decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.only(
                       topLeft: Radius.circular(20),
                       topRight: Radius.circular(20))
               ),
+              child: TasksList(),
             ),
           ),
         ],
