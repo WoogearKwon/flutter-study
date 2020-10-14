49f6d5f96e929fc8e49e84eb417c79f579ad96a2
Woogear Kwon <techkwon1717@hanmail.net>
2020-10-14 22:59:24 +0900

    Add state for managing checkboxes

--- lib/page/todoey/widgets/tasks_tile.dart
+++ lib/page/todoey/widgets/tasks_tile.dart
@@ -1,13 +1,49 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
+  void checkboxCallback (bool checkboxState) {
+    setState(() {
+      isChecked = checkboxState;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('This is a task.'),
-      trailing: Checkbox(
-        value: false,
+      title: Text(
+        'This is a task.',
+        style: TextStyle(
+            decoration: isChecked ? TextDecoration.lineThrough : null),
       ),
+      trailing: TaskCheckbox(
+          checkboxState: isChecked,
+          toggleCheckboxState: checkboxCallback),
+    );
+  }
+}
+
+// TaskCheckbox doesn't need to be a StatefulWidget because TaskTile is Stateful.
+// Every time user clicks the check box TaskTile's state changes.
+// So it builds a brand new widget on a variable #checkboxState.
+class TaskCheckbox extends StatelessWidget {
+  final bool checkboxState;
+  final Function toggleCheckboxState;
+
+  TaskCheckbox({this.checkboxState, this.toggleCheckboxState});
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: checkboxState,
+      onChanged: toggleCheckboxState
     );
   }
 }
